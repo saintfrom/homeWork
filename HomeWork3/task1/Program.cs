99f808c2248e687891238e3a33c332d6906096f0
Vlad <saintfrom@gmail.com>
2022-12-15 15:27:46 +0300

    first

---
+++ HomeWork3/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание №1
+            Console.WriteLine("Введите число: ");
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Введенное число - четное");
+            }
+            else
+                Console.WriteLine("Введенное число - нечетное");
+        }
+    }
+}
