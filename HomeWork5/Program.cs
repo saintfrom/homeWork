d085d85691660b6c5e2e961d7a34ba77ef7b0972
Vlad <saintfrom@gmail.com>
2023-11-22 13:03:50 +0300

    hw5

---
+++ HomeWork5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст: ");
+            PrintText(SplitText(Console.ReadLine()));
+        }
+        static string[] SplitText(string text)
+        {
+            string[] splitedText = text.Split(' ');
+            return splitedText;
+        }
+        static void PrintText(string[] splitedText)
+        {
+            foreach (string word in splitedText)
+                Console.WriteLine($"Вывод каждого слова в отдельной строке: {word}");
+        }
+    }
+}
